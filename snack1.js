522944ffeb5bf012c98f5b2907df3001a6be43fe
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2025-03-13 10:40:06 +0100

    feat milestone 2

--- snack1.js
+++ snack1.js
@@ -1,4 +1,11 @@
+// Milestone 1
 function Somma(x, y) {
   return x + y;
 }
 console.log(Somma(5, 3));
+
+// Milestone 2
+let Addizione = function (a, b) {
+  return a + b;
+};
+console.log(Addizione(6, 4));
